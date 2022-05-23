ec026007e295aaa33a2ea7e678823501a6b5d816
Max Potasio <simarxpotasio@gmail.com>
2022-05-23 19:11:08 -0300

    Testing invalid inputs.

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,6 +1,24 @@
 import request from "supertest";
 import { app } from "../../app";
 
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test.com", password: "password" })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "p" })
+    .expect(400);
+});
+
+it("returns a 400 with missing email or password", async () => {
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
+
 it("returns a 201 on successful sigup", async () => {
   return request(app)
     .post("/api/users/signup")
