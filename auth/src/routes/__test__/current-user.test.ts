67f59ed8fdc315701245c5f49cd69d194af93440
Max Potasio <simarxpotasio@gmail.com>
2022-05-25 19:00:54 -0300

    testing non-authed requests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,8 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
 
-const globalAny: any = global;
-
 it("responds with details about the current user", async () => {
   const cookie = await global.signin();
 
@@ -13,4 +11,13 @@ it("responds with details about the current user", async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
